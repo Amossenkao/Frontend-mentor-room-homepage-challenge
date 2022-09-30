464c38cb10ff7c7b69489a9279ce4d0f98131b51
Amos <109663323+Amossenkao@users.noreply.github.com>
2022-10-01 00:25:27 +0900

    Added some minor changes

--- script.js
+++ script.js
@@ -4,18 +4,36 @@ let isDesktop, isTablet;
 let activeSlide = 1;
 
 const heroImage = document.querySelector('.hero-img');
+const textContainer = document.querySelector('.section-1 .text-container');
 const heading = document.querySelector('.heading-1');
 const text = document.querySelector('.text-1');
 const desktopScreen = matchMedia('(min-width: 1025px)');
 const tabletScreen = matchMedia('(min-width: 601px) and (max-width: 1024px)');
+let slideButtons;
 
 function checkScreenSize() {
 	isDesktop = desktopScreen.matches ? true : false;
 	isTablet = tabletScreen.matches ? true : false;
 	if (isDesktop || isTablet) {
 		heroImage.src = pageData[1].heroImage.desktop;
+		if (isDesktop) {
+			const desktopSlideButtons = document.createElement('div');
+			desktopSlideButtons.className = 'slide-arrows desktop';
+			desktopSlideButtons.innerHTML =
+				document.querySelector('.slide-arrows').innerHTML;
+			document
+				.querySelector('.section-1 .text-button')
+				.append(desktopSlideButtons);
+		} else {
+			try {
+				document.querySelector('slide-arrows.desktop').remove();
+			} catch {}
+		}
 	} else {
 		heroImage.src = pageData[1].heroImage.tabletMobile;
+		try {
+			document.querySelector('slide-arrows.desktop').remove();
+		} catch {}
 	}
 }
 
@@ -24,7 +42,9 @@ desktopScreen.addListener(checkScreenSize);
 tabletScreen.addListener(checkScreenSize);
 
 const toggleNavButtons = [...document.querySelectorAll('.menu, .close')];
-const slideAngles = [...document.querySelectorAll('.slide-arrows > *')];
+slideButtons = isDesktop
+	? [...document.querySelectorAll('.slide-arrows.desktop > *')]
+	: [...document.querySelectorAll('.slide-arrows > *')];
 const navMenu = [...document.querySelectorAll('.nav-menu > li')];
 
 toggleNavButtons.forEach((btn) => {
@@ -63,113 +83,107 @@ document.addEventListener('click', (event) => {
 	}
 });
 
-function resetAnimation() {
-	heroImage.classList.remove('animate-left', 'animate-right');
-	heading.classList.remove('animate', 'animate-left', 'animate-right');
-	text.classList.remove('animate', 'animate-left', 'animate-right');
+function getTtransformProperty(element) {
+	return element.style.transform === ''
+		? 0
+		: Number(element.style.transform.split('(')[1].split('%')[0]);
 }
 
-slideAngles.forEach((button, index) => {
-	button.addEventListener('mousedown', resetAnimation);
-
+slideButtons.forEach((button, index) => {
 	button.addEventListener('click', (event) => {
+		let transform = getTtransformProperty(heroImage);
 		if (document.querySelector('.close').classList.contains('active')) {
 			event.preventDefault();
+		} else if (
+			button.classList.contains('angle-left') &&
+			transform <= -200
+		) {
+			event.preventDefault();
+		} else if (button.classList.contains('angle-right') && transform >= 0) {
+			event.preventDefault();
 		} else {
-			clickHandler(index);
+			if (index === 0) {
+				document
+					.querySelectorAll('.hero-img, .section-1 .text-container')
+					.forEach((img) => {
+						img.style.transform = `translate(${transform - 100}%)`;
+					});
+			} else {
+				document
+					.querySelectorAll('.hero-img, .section-1 .text-container')
+					.forEach((img) => {
+						img.style.transform = `translate(${transform + 100}%)`;
+					});
+			}
 		}
 	});
 });
 
-function clickHandler(index) {
-	const heroImage = document.querySelector('.hero-img');
-
-	if (isDesktop) {
-		heroImage.classList.add('animate-left');
-		heading.classList.add('animate');
-		text.classList.add('animate');
-	} else {
-		if (index === 0) {
-			heroImage.classList.add('animate-left');
-			heading.classList.add('animate-left');
-			text.classList.add('animate-left');
+function appendElenemts() {
+	for (let i = 2; i <= 3; i++) {
+		const imageElement = document.createElement('img');
+		const textElement = document.createElement('div');
+		textElement.innerHTML = textContainer.innerHTML;
+		textElement.querySelector('.heading-1').textContent =
+			pageData[i].textSection.heading;
+		textElement.querySelector('.text-1').textContent =
+			pageData[i].textSection.text;
+		imageElement.className = 'hero-img';
+		textElement.className = 'text-container';
+		if (isDesktop || isTablet) {
+			imageElement.src = pageData[i].heroImage.desktop;
 		} else {
-			heroImage.classList.add('animate-right');
-			heading.classList.add('animate-right');
-			text.classList.add('animate-right');
+			imageElement.src = pageData[i].heroImage.tabletMobile;
 		}
+
+		heroImage.parentElement.append(imageElement);
+		textContainer.parentElement.append(textElement);
 	}
-	let dataIndex;
-
-	setTimeout(() => {
-		if (isDesktop) {
-			dataIndex = (activeSlide % 3) + 1;
-			heroImage.src = pageData[dataIndex].heroImage.desktop;
-			activeSlide = pageData[dataIndex].slideNumber;
-		} else {
-			if (index === 0) {
-				if (activeSlide === 1) {
-					dataIndex = 3;
-				} else {
-					dataIndex = activeSlide - 1;
-				}
-			} else {
-				if (activeSlide === 3) {
-					dataIndex = 1;
-				} else {
-					dataIndex = activeSlide + 1;
-				}
-			}
-
-			heroImage.src = pageData[dataIndex].heroImage.tabletMobile;
-			activeSlide = pageData[dataIndex].slideNumber;
-		}
-
-		heading.textContent = pageData[dataIndex].textSection.heading;
-		text.textContent = pageData[dataIndex].textSection.text;
-	}, 375);
 }
+
+appendElenemts();
 
 // Swipe events for touch devices 👇👇
 
 function touchEventHandler() {
 	const sectionOne = document.querySelector('.section-1');
 	let startingX, startingY, changesX, changesY, swapStartTime, timeDifference;
-	sectionOne.addEventListener('touchstart', (event) => {
-		resetAnimation();
-		startingX = event.touches[0].pageX;
-		startingY = event.touches[0].pageY;
-		swapStartTime = event.timeStamp;
-	});
+	document.querySelectorAll('.hero-img').forEach((image) => {
+		image.addEventListener('touchstart', (event) => {
+			startingX = event.touches[0].pageX;
+			startingY = event.touches[0].pageY;
+			swapStartTime = event.timeStamp;
+		});
 
-	sectionOne.addEventListener('touchmove', (event) => {
-		changesX = startingX - event.touches[0].pageX;
-		changesY = startingY - event.touches[0].pageY;
-	});
+		image.addEventListener('touchmove', (event) => {
+			changesX = startingX - event.touches[0].pageX;
+			changesY = startingY - event.touches[0].pageY;
+		});
 
-	sectionOne.addEventListener('touchend', (event) => {
-		timeDifference = event.timeStamp - swapStartTime;
-		if (changesX > 0) {
-			if (
-				changesX > changesY &&
-				changesY < 75 &&
-				changesY > -75 &&
-				changesX >= 75 &&
-				timeDifference <= 500
-			) {
-				slideAngles[0].click();
+		image.addEventListener('touchend', (event) => {
+			timeDifference = event.timeStamp - swapStartTime;
+			if (changesX > 0) {
+				if (
+					changesX > changesY &&
+					changesY < 75 &&
+					changesY > -75 &&
+					changesX >= 50 &&
+					timeDifference <= 500
+				) {
+					slideButtons[0].click();
+				}
+			} else if (changesX < 0) {
+				if (
+					changesX < changesY &&
+					changesY > -75 &&
+					changesY < 75 &&
+					changesX <= -50 &&
+					timeDifference <= 500
+				) {
+					slideButtons[1].click();
+				}
 			}
-		} else if (changesX < 0) {
-			if (
-				changesX < changesY &&
-				changesY > -75 &&
-				changesY < 75 &&
-				changesX <= -75 &&
-				timeDifference <= 500
-			) {
-				slideAngles[1].click();
-			}
-		}
+		});
 	});
 }
 
